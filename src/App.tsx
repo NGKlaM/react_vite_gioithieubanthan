af8f29e1fc81ebae0bd8d9dfc36fab6acbaea0de
lamngk <lamnkph28782@fpt.edu.vn>
2023-11-10 18:47:20 +0700

    code

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className="container">
+    <div className="box">
+      <h1>
+        Hello
+      </h1>
+      <img src="public/hinh-nen-may-tinh-4k-cong-nghe-4.jpg" alt="" />
+      <div className="text">
+        <p>Họ Tên: Nguyễn Kiều Lâm</p>
+        <p>Tuổi: 21</p>
+        <p>Quê Quán: Ba Vì Hà Nội</p>
+        <p>Định Hướng: Full-stack</p>
+        <p>Ngôn ngũ đã học: Js, java, C# ,PHP</p>
+      </div>
+    </div>
+  </div>
+    </>
+  )
+}
+
+export default App
